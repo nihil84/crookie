4f5e71b360ba5ecd266e20043712bbc545411847
Paolo Bernini <paolo@McGyver.local>
2014-03-15 18:35:53 +0100

    [ADD] crookie: empty Handle class header

---
+++ Handle.h
@@ -0,0 +1,14 @@
+//
+//  Handle.h
+//  crookie
+//
+//  Created by Paolo Bernini on 15/03/14.
+//  Copyright (c) 2014 Paolo Bernini. All rights reserved.
+//
+
+#ifndef crookie_Handle_h
+#define crookie_Handle_h
+
+
+
+#endif
