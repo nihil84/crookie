78abf1f6c78ece55e8b2e9a631e7cb1427a43a6b
Paolo Bernini <nihil84@gmail.com>
2016-08-29 12:58:05 +0200

    [ADD] EventBus: add compile-time and run-time checks for EventBase derivation correctness

--- EventBus/EventBase.h
+++ EventBus/EventBase.h
@@ -12,8 +12,9 @@
 #include "Core/IEvent.h"
 #include "AEventHandler.h"
 
-#include <cassert>
 #include <type_traits>
+#include <cassert>
+
 
 namespace crookie {
   
@@ -23,21 +24,45 @@ namespace crookie {
  * Any other detail of MyEvent class (constructor parameters, public methods,
  * data members, etc...) is up to you.
  * 
- * @tparm EventClass Type of the event derived from this template instance.
+ * @tparm EventClass Type of derived class from the instance of the template.
  */
 template <class EventClass>
 class EventBase : public IEvent
 {
 public:
   
+  typedef EventClass DerivedEvent;
+  
   typedef AEventHandler<EventClass> SpecificHandler;
   
   static const int TYPE;
   
-  virtual int type() const { return TYPE; }
   
-  virtual void dispatch(IEventDispatcher& handler) const
+  //! Constructor is here mainly for type checks.
+  EventBase()
   {
+    using SpecializedType = typename std::remove_pointer<decltype(this)>::type;
+  
+    // The following assert is here to check that the declaration of the Event
+    // class is correct: class YourEvent : public EventBase<YourEvent>
+    // If the assert fails you provided the wrong template argument to EventBase
+    // Unfortunately the static_assert will be deceived by the corner but common
+    // case of putting another class which is actually another derivation of
+    // EventBase. That is the reason behind the assert in the dispatch method.
+    static_assert(std::is_base_of<SpecializedType, DerivedEvent>::value,
+                  "EventBase template specialization error: "
+                  "EventBase<EventClass> must be the base class of EventClass");
+  }
+
+  virtual int type() const override { return TYPE; }
+  
+  virtual void dispatch(IEventDispatcher& handler) const override
+  {
+    // The following assert is here to check that the declaration of the Event
+    // class is correct: class YourEvent : public EventBase<YourEvent>
+    // If the assert fails you provided the wrong template argument to EventBase
+    assert(dynamic_cast<const DerivedEvent*>(this) != nullptr);
+    
     SpecificHandler& specific = dynamic_cast<SpecificHandler&>(handler);
     specific.onEvent(static_cast<const EventClass&>(*this));
   }
