6672a955f846e529e91f5672e39829b7386738d5
Paolo Bernini <nihil84@gmail.com>
2016-10-23 17:32:03 +0200

    [ADD] documentation and an additional test case for ActiveObject

--- EventBus/ActiveObject.h
+++ EventBus/ActiveObject.h
@@ -9,53 +9,70 @@
 
 
 namespace crookie {
-
   
 /**
- * @brief
- * 
+ * @brief This class is a basic implementation of the active object design 
+ * pattern that easily matches most use cases of the pattern.
+ *
+ * From wikipedia: The active object design pattern decouples method execution 
+ * from method invocation for objects that each reside in their own thread of 
+ * control. The goal is to introduce concurrency, by using asynchronous method 
+ * invocation and a scheduler for handling requests.
+ *
+ * This class implements a scheduler running in its own thread of execution,
+ * while the asynchronous methods are the event handlers implemented in the
+ * derived class and distiguished each others thanks to the type of the event 
+ * argument.
  */
 template <class... EventTypes>
 class ActiveObject : public AEventHandler<EventTypes>...
 {
 public:
   
+  //! Convenient alias for derived classes
+  typedef ActiveObject<EventTypes...> ObjectBase;
+  
+  //! Type of the queue of events (internal use only)
   typedef std::list< Event > EventsQueue;
-
-  ActiveObject() { }
   
-  ActiveObject(const ActiveObject&) = delete;
-  
+  //! Constructor
+  //! @param [in] bus   The bus on which subscribe for handled events
   explicit ActiveObject(EventBus& bus)
     : AEventHandler<EventTypes>(bus)...
     , m_done(false)
   { }
 
-  ~ActiveObject()
+  //! Desctructor
+  ~ActiveObject() noexcept
   {
     this->quit();
     this->join();
   }
   
+  //! @brief Handler function, needed for EventBus integration: do not override.
   virtual void handle(const Event& event) override
   {
     std::unique_lock<std::mutex> lock(m_mutex);
     m_queue.push_back(event);
   }
   
+  //! @brief Starts the event dispatching.
   virtual void start()
   {
     if (m_thread.joinable() && m_thread.get_id() != std::this_thread::get_id())
       return;
     
+    m_done = false;
     m_thread = std::thread(&ActiveObject::run, this);
   }
   
+  //! @brief Stops events dispatching.
   void quit()
   {
     m_done = true;
   }
   
+  //! @brief Waits for dispatching thread termination.
   void join()
   {
     if (m_thread.joinable())
@@ -64,8 +81,12 @@ public:
 
 protected:
   
+  //! @brief Return true when the quit() has been called.
+  //! @details Useful for derived classes to know if thread termination has
+  //! been requested
   bool done() { return m_done; }
   
+  //! @brief Thread main loop, dispatching events.
   virtual void run()
   {
     while (!done())
@@ -74,6 +95,7 @@ protected:
     }
   }
   
+  //! @brief Extracts events from the queue and dispatch them, one at time.
   void dispatch()
   {
     EventsQueue events;
@@ -81,7 +103,7 @@ protected:
       std::unique_lock<std::mutex> lock(m_mutex);
       events.swap(m_queue);
     }
-    
+
     while (events.size() > 0) try
     {
       Event event = events.front();
@@ -113,10 +135,13 @@ protected:
   
 private:
 
-  std::mutex m_mutex;
-  EventsQueue m_queue;
-  std::thread m_thread;
-  std::atomic<bool> m_done;
+  std::mutex m_mutex;       //!< mutex for queue access
+  EventsQueue m_queue;      //!< queue of events
+  std::thread m_thread;     //!< thread object
+  std::atomic<bool> m_done; //!< flag for thread termination
+  
+  ActiveObject(const ActiveObject&) = delete;
+  ActiveObject& operator =(const ActiveObject&) = delete;
 };
   
   
--- test/test_ActiveObject.cpp
+++ test/test_ActiveObject.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <stdexcept>
 #include <cassert>
 
 using namespace std;
@@ -34,7 +35,8 @@ public:
 class AnEvent : public EventBase<AnEvent>
 {
 public:
-  AnEvent() {}
+  AnEvent(int n) : n(n) {}
+  int n;
 };
 
 
@@ -58,14 +60,36 @@ public:
   
   void onEvent(const AnEvent& evt)
   {
+    if (evt.n != m_ordered)
+    {
+      throw std::logic_error("an event has been receieved out of order");
+    }
     
+    m_ordered++;
+    
+    if (evt.n == 1)
+    {
+      throw std::runtime_error("let's start with an error");
+    }
   }
   
   bool receivedSimple() const { return m_simple; }
   
+  int count() const { return m_ordered; }
+  
 private:
   
   bool m_simple = false;
+  int m_ordered = 0;
+  
+  virtual void run()
+  {
+    for (;;)
+    {
+      try { ObjectBase::run(); break; }
+      catch (std::runtime_error& ex) { } // intercept runtime_error only
+    }
+  }
 };
 
 
@@ -93,6 +117,7 @@ BOOST_AUTO_TEST_CASE( test_concurrent_delivery )
   
   m_bus->dispatch<SimpleEvent>();
   
+  // checks handling does not happen if thread is off
   BOOST_CHECK( !handler.receivedSimple() );
   
   handler.start();
@@ -103,8 +128,54 @@ BOOST_AUTO_TEST_CASE( test_concurrent_delivery )
     ;
   
   handler.quit();
+  handler.join();
   
   BOOST_CHECK( handler.receivedSimple() );
+}
+
+//------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE( test_activeobject_resume_after_exception )
+{
+  ActiveHandler handler(*m_bus);
+  
+  try
+  {
+    m_bus->dispatch<SimpleEvent>();
+    m_bus->dispatch<AnEvent>(0);
+    m_bus->dispatch<AnEvent>(1);
+    
+    // dispatching will be interrupted here by the runtime_error exception
+    // but it should be resumed in the same order, otherwise an unhandled
+    // logic_error exception will be caught from the catch below, failing the
+    // test
+    
+    m_bus->dispatch<AnEvent>(2);
+    m_bus->dispatch<AnEvent>(3);
+    m_bus->dispatch<AnEvent>(4);
+    
+    BOOST_CHECK(!handler.receivedSimple());
+    BOOST_CHECK(handler.count() == 0);
+    
+    handler.start();
+    
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    
+    while (handler.count() < 5)
+      ;
+    
+    handler.quit();
+    handler.join();
+
+    BOOST_CHECK(handler.receivedSimple());
+    BOOST_CHECK(handler.count() == 5);
+  }
+  catch (std::exception& ex)
+  {
+    // exception shall not be propagated here
+    BOOST_TEST_MESSAGE(std::string("unexpected exception: ") + ex.what());
+    
+    BOOST_CHECK(false);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
